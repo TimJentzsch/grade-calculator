ccee5ba90dece52262dfdf665ce73526b4d5e2c6
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-05-14 20:04:06 +0200

    Add module function tests

--- src/scripts/module/module.test.js
+++ src/scripts/module/module.test.js
@@ -92,4 +92,123 @@ describe('Module', () => {
       });
     });
   });
+  // --- Functions ---
+  describe('functions', () => {
+    // Clone
+    describe('clone', () => {
+      test('equality for graded module', () => {
+        const module = new Module('Test module', 5, 2.3, 3, undefined, undefined);
+        expect(module.clone()).toEqual(module);
+      });
+      test('equality for ungraded module', () => {
+        const module = new Module('Test module', 5, undefined, 3, true, true);
+        expect(module.clone()).toEqual(module);
+      });
+    });
+    // Best case
+    describe('bestCase', () => {
+      test('Incomplete graded module recieves 1.0 as grade', () => {
+        const module = new Module('Test module', 5, undefined, 3, undefined, undefined);
+        expect(module.bestCase().grade).toBe(1.0);
+      });
+      test('no changes for module with grade', () => {
+        const module = new Module('Test module', 5, 2.3, 3, undefined, undefined);
+        expect(module.bestCase()).toEqual(module);
+      });
+      test('no changes for ungraded module', () => {
+        const module = new Module('Test module', 5, undefined, 3, true, true);
+        expect(module.bestCase()).toEqual(module);
+      });
+    });
+    // Worst case
+    describe('worstCase', () => {
+      test('Incomplete graded module recieves 4.0 as grade', () => {
+        const module = new Module('Test module', 5, undefined, 3, undefined, undefined);
+        expect(module.worstCase().grade).toBe(4.0);
+      });
+      test('no changes for module with grade', () => {
+        const module = new Module('Test module', 5, 2.3, 3, undefined, undefined);
+        expect(module.worstCase()).toEqual(module);
+      });
+      test('no changes for ungraded module', () => {
+        const module = new Module('Test module', 5, undefined, 3, true, true);
+        expect(module.worstCase()).toEqual(module);
+      });
+    });
+    // To object
+    describe('toObject', () => {
+      test('complete graded module', () => {
+        const module = new Module('Test module', 5, 2.3, 3, undefined, undefined);
+        const expectedObj = {
+          name: 'Test module',
+          credits: 5,
+          grade: 2.3,
+          weight: 3,
+        };
+        expect(module.toObject()).toEqual(expectedObj);
+      });
+      test('incomplete ungraded module', () => {
+        const module = new Module('Test module', 5, undefined, 3, undefined, undefined);
+        const expectedObj = {
+          name: 'Test module',
+          credits: 5,
+          weight: 3,
+        };
+        expect(module.toObject()).toEqual(expectedObj);
+      });
+      test('complete ungraded module', () => {
+        const module = new Module('Test module', 5, undefined, 3, true, true);
+        const expectedObj = {
+          name: 'Test module',
+          credits: 5,
+          weight: 3,
+          notGraded: true,
+          passed: true,
+        };
+        expect(module.toObject()).toEqual(expectedObj);
+      });
+      test('incomplete ungraded module', () => {
+        const module = new Module('Test module', 5, undefined, 3, true, undefined);
+        const expectedObj = {
+          name: 'Test module',
+          credits: 5,
+          weight: 3,
+          notGraded: true,
+        };
+        expect(module.toObject()).toEqual(expectedObj);
+      });
+      test('default weight deleted', () => {
+        const module = new Module('Test module', 5, undefined, 1, true, undefined);
+        expect(module.toObject().weight).toBeUndefined();
+      });
+      test('graded module annotation deleted', () => {
+        const module = new Module('Test module', 5, 2.3, 1, undefined, undefined);
+        expect(module.toObject().notGraded).toBeUndefined();
+      });
+    });
+    // From object
+    describe('fromObject', () => {
+      test('from graded object', () => {
+        const obj = {
+          name: 'Test module',
+          credits: 5,
+          grade: 2.3,
+          weight: 3,
+        };
+        const expectedModule = new Module('Test module', 5, 2.3, 3, undefined, undefined);
+        expect(Module.fromObject(obj)).toEqual(expectedModule);
+      });
+      test('from ungraded object', () => {
+        const obj = {
+          name: 'Test module',
+          credits: 5,
+          weight: 3,
+          notGraded: true,
+          passed: true,
+        };
+        const expectedModule = new Module('Test module', 5, undefined, 3, true, true);
+        expect(Module.fromObject(obj)).toEqual(expectedModule);
+      });
+    });
+  });
 });
